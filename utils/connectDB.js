577cd0b82129d0ee7bed02bb82ff5ef5d7bf57b9
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2023-06-16 18:58:51 +0530

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,19 +1,17 @@
 import mongoose from 'mongoose';
 
-const connectDB = () => {
-    if (mongoose.connections[0].readyState) {
-        console.log('Already connected!');
-        return;
-    }
-    mongoose.set('strictQuery', false);
-    const mongodbURI = 'mongodb+srv://aditya4sure:RiseAbove@a3mportal.jthdt9a.mongodb.net/?retryWrites=true&w=majority';
-    mongoose.connect(mongodbURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, (err) => {
-        if (err) throw err;
-        console.log('Connected to MongoDB!');
+// Update your connectDB function
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://aditya4sure:RiseAbove@a3mportal.jthdt9a.mongodb.net/?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+  }
 };
 
-export default connectDB;
+// Call the updated connectDB function
+connectDB();
