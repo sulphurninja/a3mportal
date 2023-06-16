6d78d2e33fd552f301c144ba53369549b09aec92
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2023-06-16 18:57:05 +0530

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,18 +1,19 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const connectDB = () => {
     if (mongoose.connections[0].readyState) {
-        console.log('Already connected Biatch!')
+        console.log('Already connected!');
         return;
     }
-    mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGODB_URI, {
+    mongoose.set('strictQuery', false);
+    const mongodbURI = 'mongodb+srv://aditya4sure:RiseAbove@a3mportal.jthdt9a.mongodb.net/?retryWrites=true&w=majority';
+    mongoose.connect(mongodbURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    }, err => {
+    }, (err) => {
         if (err) throw err;
-        console.log("Connected to Mongodb bitch!")
-    })
-}
+        console.log('Connected to MongoDB!');
+    });
+};
 
-export default connectDB
+export default connectDB;
