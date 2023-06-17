0a3fb8034c370cfeaa3888cc89530963fb202b42
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2023-06-17 23:51:34 +0530

    Update getUsers.js

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
     try {
       const users = await User.find();
       res.status(200).json(users);
-      console.log(tasks);
     } catch (error) {
       res.status(500).json({ error: 'Internal server error' });
     }
